6d514d14356971f69bee6f877b85a1251b205240
Passadech <passadech.chot@bumail.net>
2023-12-03 22:48:21 +0700

    LoginFrame - Update database and textfeild -- NOT ALREADY CONNECT XAMMP SERVER --

---
+++ src/main/java/com/mycompany/project/CustomFont.java
@@ -0,0 +1,36 @@
+package com.mycompany.project;
+
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.File;
+import java.io.IOException;
+
+public class CustomFont {
+    private Font customFont;
+
+    public CustomFont() {
+        this.customFont = null;
+    }
+
+    public Font setFontFile(int fontSize) {
+        try {
+            File customFontFile = getFontFile("/Jua-Regular.ttf");
+            // Create custom font
+            customFont = Font.createFont(Font.TRUETYPE_FONT, customFontFile).deriveFont((float) fontSize);
+        } catch (FontFormatException | IOException e) {
+            System.out.println("Error: " + e);
+        }
+        return customFont;
+    }
+
+    public static File getFontFile(String resource) {
+        String filePath = LoginFrame.class.getResource(resource).getPath();
+
+        // Check to see if filepath contains any folder with spaces in the name
+        if (filePath.contains("%20")) {
+            filePath = filePath.replaceAll("%20", " ");
+        }
+
+        return new File(filePath);
+    }
+}

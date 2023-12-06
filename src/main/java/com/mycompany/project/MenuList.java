3df17a4264a7c9047e037f93db0ac414d25b740b
I3Estz <navapan.suthon123@gmail.com>
2023-12-06 14:13:17 +0700

    Update Select Boiled MenuFinished

--- src/main/java/com/mycompany/project/MenuList.java
+++ src/main/java/com/mycompany/project/MenuList.java
@@ -6,14 +6,15 @@ import java.util.*;
 
 //--JAVA CLASS--..
 public class MenuList {
-    
-    //--METHOD--..
-    public ArrayList<String> SelectMenu = new ArrayList<>();
+    public ArrayList<String> SelectMenuList = new ArrayList<>();
 
-    public ArrayList<String> BoiledMenu = new ArrayList<>(Arrays.asList(
+    public ArrayList<String> BoiledMenuList = new ArrayList<>(Arrays.asList(
             "KuayJapYuan.png","KuayTiewGai.png","KuayTiewYenTaFo.png","KuayTiewMoo.png",
             "KhaoKaMoo.png","KhaoManGai.png","MakkaroniPadSoyaGai.png","MakkaroniPadSoyaTaLay.png",
             "MakkaroniPadSoyaMoo.png","SpaghettiKhaiMawGai.png","SpaghettiKhaiMawTaLay.png","SpaghettiKhaiMawMoo.png",
             "SukiyakiGai.png","SukiyakiTaLay.png","SukiyakiMoo.png","KuayTiewMooTomYum.png",
-            "Yum Mama.png"));
+            "YumMama.png"));
+    
+    public ArrayList<String> StirFriedMenuList = new ArrayList<>(Arrays.asList("wow"));
+    public ArrayList<String> FriedMenuList = new ArrayList<>(Arrays.asList("wow"));
 }
